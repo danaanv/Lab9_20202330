aba4ca56f2eb37dd43e8b3b48b6097840e426983
sebas1310 <a20203368@pucp.edu.pe>
2023-06-20 19:06:30 -0500

    Initial commit

---
+++ src/main/java/com/example/lab9_20202330/Lab920202330Application.java
@@ -0,0 +1,13 @@
+package com.example.lab9_20202330;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab920202330Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab920202330Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab9_20202330/Lab920202330ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab9_20202330;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab920202330ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
